df493bfe458f9b0f33d439b12d1ecaa70fe94cf4
Neel <ineelsandilya@gmail.com>
2025-11-20 15:53:17 +0530

    allowed frontend cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,7 +12,7 @@ import 'dotenv/config';
 app.use(express.json({limit:"5mb"}));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chatapp-front-b7la.onrender.com",
   credentials: true,
 }));
 
