6d4f375e9b01666ef92cd676ce7a413e7a22cd50
Neel <ineelsandilya@gmail.com>
2025-11-20 15:24:54 +0530

    cmt 2

--- Frontend/chatfrontend/src/socket.js
+++ Frontend/chatfrontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 
-export const socket =io("http://localhost:5000", {
+export const socket =io(import.meta.env.MODE =="development"?"http://localhost:5000":import.meta.env.VITE_API_URL, {
   autoConnect: false,
   withCredentials: true,
 });
