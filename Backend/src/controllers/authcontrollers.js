e118d156ec52b92b627d2adc998e43de2d2b9304
Neel <ineelsandilya@gmail.com>
2025-11-20 18:38:21 +0530

    cmt34

--- Backend/src/controllers/authcontrollers.js
+++ Backend/src/controllers/authcontrollers.js
@@ -38,7 +38,8 @@ export const signup =async (req,res)=>{
             res.cookie("jwt",token,{maxAge: 7*24*60*60*1000,
                 httpOnly:true,
                 sameSite:"none",
-                secure: process.env.NODE_ENV === "production"
+                secure: true,
+                // secure: process.env.NODE_ENV === "production"
 
                 
             } )
--- Frontend/chatfrontend/src/socket.js
+++ Frontend/chatfrontend/src/socket.js
@@ -4,5 +4,7 @@ import { io } from "socket.io-client";
 export const socket =io(import.meta.env.MODE =="development"?"http://localhost:5000":import.meta.env.VITE_API_URL, {
   autoConnect: false,
   withCredentials: true,
+    transports: ["websocket", "polling"]
+
 });
 
